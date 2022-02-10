05e46dd66c236db26e3e1fc44e3b1f190ff50fea
Poonam <anmolpreetsmailbox@gmail.com>
2022-02-10 17:11:04 +0530

    Filer Demo -Input validation

---
+++ src/main/java/com/RegisterServlet.java
@@ -0,0 +1,35 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RegisterServlet
+ */
+public class RegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public RegisterServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out=response.getWriter();
+		System.out.println("Hello from servlet");
+		out.println("<H4> Pan number "+request.getAttribute("pan")+" Validated Successfully</H4>");
+		System.out.println("Bye from Servlet");
+		
+	}
+
+}
